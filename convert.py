5e696d1e37a17c2a780343dd2d64a819159b5627
ianbarto-cs <treuxxz@icloud.com>
2025-09-03 10:02:25 -0600

    Final-1

--- convert.py
+++ convert.py
@@ -1,27 +1,27 @@
 starting_temp = input("What temperature are you starting with: Kelvin(K) Fahrenheit(F) Celsius(C)")
-starting_value = input("What is the value of your current temperature?")
+starting_value = float(input("What is the value of your current temperature?"))
 converting_temp = input("What are you converting your temperature to: Kelvin(K) Fahrenheit(F) Celsius(C)")
 
-if starting_temp == ("f") or starting_temp == ("F"):
-    if converting_temp == ("k") or converting_temp == ("K"):
-        Answer = (int(starting_value) - 32) * 5/9 + 273.15
+if starting_temp.lower() == ("f"):
+    if converting_temp.lower() == ("k"):
+        Answer = (starting_value - 32) * 5/9 + 273.15
         print (f" {starting_value} is {Answer} in Kelvin")
-    elif converting_temp == ("c") or converting_temp == ("C"):
-        Answer = (int(starting_value)-32) * 5/9
+    elif converting_temp.lower() == ("c"):
+        Answer = (starting_value - 32) * 5/9
         print (f" {starting_value} is {Answer} in Celsius")
-elif starting_temp == ("K") or starting_temp == ("k"):
-    if converting_temp == ("f") or converting_temp ("F"):
-        Answer = (int(starting_value) - 273.15) * 9/5 + 32
+elif starting_temp.lower() == ("k"):
+    if converting_temp.lower() == ("f"):
+        Answer = (starting_value - 273.15) * 9/5 + 32
         print (f" {starting_value} is {Answer} in Fahrenheit")
-    elif converting_temp == ("c") or converting_temp == ("C"):
-        Answer = (int(starting_value) - 273.15)
+    elif converting_temp.lower() == ("c"):
+        Answer = (starting_value - 273.15)
         print (f" {starting_value} is {Answer} in Celsius")
-elif starting_temp == ("c") or starting_temp == ("C"):
-    if converting_temp == ("f") or converting_temp ("F"):
-        Answer = (int(starting_value) * 9/5) + 32
+elif starting_temp.lower() == ("c"):
+    if converting_temp.lower() == ("f"):
+        Answer = (starting_value * 9/5) + 32
         print (f" {starting_value} is {Answer} in Fahrenheit")
-    elif converting_temp == ("k") or converting_temp == ("K"):
-        Answer = (int(starting_value)+ 273.15)
+    elif converting_temp.lower() == ("k"):
+        Answer = (starting_value + 273.15)
         print (f" {starting_value} is {Answer} in Kelvin")
         
 
