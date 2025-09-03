2fd3bd71619481a4c1708596763f4d6a890d28c2
ianbarto-cs <treuxxz@icloud.com>
2025-09-03 10:16:16 -0600

    Final

--- convert.py
+++ convert.py
@@ -5,24 +5,28 @@ converting_temp = input("What are you converting your temperature to: Kelvin(K) 
 if starting_temp.lower() == ("f"):
     if converting_temp.lower() == ("k"):
         Answer = (starting_value - 32) * 5/9 + 273.15
-        print (f" {starting_value} is {Answer} in Kelvin")
+        print (f" {starting_value}°F is {Answer}°K")
     elif converting_temp.lower() == ("c"):
         Answer = (starting_value - 32) * 5/9
-        print (f" {starting_value} is {Answer} in Celsius")
+        print (f" {starting_value}°F is {Answer}°C")
 elif starting_temp.lower() == ("k"):
     if converting_temp.lower() == ("f"):
         Answer = (starting_value - 273.15) * 9/5 + 32
-        print (f" {starting_value} is {Answer} in Fahrenheit")
+        print (f" {starting_value}°K is {Answer}°F")
     elif converting_temp.lower() == ("c"):
         Answer = (starting_value - 273.15)
-        print (f" {starting_value} is {Answer} in Celsius")
+        print (f" {starting_value}°K is {Answer}°C")
 elif starting_temp.lower() == ("c"):
     if converting_temp.lower() == ("f"):
         Answer = (starting_value * 9/5) + 32
-        print (f" {starting_value} is {Answer} in Fahrenheit")
+        print (f" {starting_value}°C is {Answer}°F")
     elif converting_temp.lower() == ("k"):
         Answer = (starting_value + 273.15)
-        print (f" {starting_value} is {Answer} in Kelvin")
+        print (f" {starting_value}°C is {Answer}°K")
+
+        
+
+
         
 
 
